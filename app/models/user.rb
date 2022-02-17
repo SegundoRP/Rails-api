a86b6cf6550f6f2790afedff9dfd699c7e38e69f
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-17 16:17:06 -0500

    api rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :restaurants
+  has_many :comments
+  acts_as_token_authenticatable
+
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
